d444336e46b3f7a300e3eb504eb19cd6f7f30658
z20ist12 <z20ist12@z00p3-10.SPSKNM>
2022-11-29 08:59:40 +0100

    Input component (2)

---
+++ src/Input.tsx
@@ -0,0 +1,8 @@
+export default function Input(){
+    return(
+        <div>
+        <input type="text" class="search-bar" placeholder="Search for a city... "/>
+        <button type="button" class="btn">submit</button>
+        </div>
+    )
+}
