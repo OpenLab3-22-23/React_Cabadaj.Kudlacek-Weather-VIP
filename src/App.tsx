f1115cb1b32ca23e2e2c654977f999a9d6bf62ca
z20ist03 <z20ist03@z00p3-09.SPSKNM>
2022-12-06 08:47:44 +0100

    css

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,24 @@ import Input from './Input'
 import Header from './Header'
 import Today from './Today'
 import Boxy from './Boxy'
+import Boxy1 from './Boxy1'
+import Boxy2 from './Boxy2'
+import Boxy3 from './Boxy3'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="container"> 
+    <div className="container">
       <Header/>
       <Input/>
       <Today/>
+      <div className="cover">  
       <Boxy den1={"Utorok"}/>
-      <Boxy den2={"Streda"}/>
-      <Boxy den3={"Stvrtok"}/>
-      <Boxy den4={"Piatok"}/>
+      <Boxy1 den2={"Streda"}/>
+      <Boxy2 den3={"Stvrtok"}/>
+      <Boxy3 den4={"Piatok"}/>
+    </div>
     </div>
   )
 }
--- src/Today.tsx
+++ src/Today.tsx
@@ -32,29 +32,7 @@ export default function Today(){
 			<div className="typeOfWeather">{weather}</div>
 		  </div>
 		  </div>
-
-			<div className="cover">
-				<div className="box">
-					<h4 className="day_1" id="day">{nameday1}</h4>
-					<img src="https://openweathermap.org/img/wn/04n.png" alt="Ikona pocasia" className="icon_1" id="icon_days" />
-					<h4 className="temp_1" id="temp">{temp1}</h4>
-				</div>
-				<div className="box">
-					<h4 className="day_2" id="day">{nameday2}</h4>
-					<img src="https://openweathermap.org/img/wn/04n.png" alt="Ikona pocasia" className="icon_2" id="icon_days" />
-					<h4 className="temp_2" id="temp">{temp2}</h4>
-				</div>
-				<div className="box">
-					<h4 className="day_3" id="day">{nameday3}</h4>
-					<img src="https://openweathermap.org/img/wn/04n.png" alt="Ikona pocasia" className="icon_3" id="icon_days" />
-					<h4 className="temp_3" id="temp">{temp3}</h4>
-				</div>
-				<div className="box">
-					<h4 className="day_4" id="day">{nameday4}</h4>
-					<img src="https://openweathermap.org/img/wn/04n.png" alt="Ikona pocasia" className="icon_4" id="icon_days" />
-					<h4 className="temp_4" id="temp">{temp4}</h4>
-				</div>
-			</div>
-			</div>
+		  </div>
+			
     )
 }
